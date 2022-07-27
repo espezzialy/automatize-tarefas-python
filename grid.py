a7c745ed53a3adddc3af79f016998178e6452530
Espezzialy Souza <espezzialy.souza-ext@ab-inbev.com>
2022-07-27 19:00:29 -0300

    feat: chapter list

--- grid.py
+++ grid.py
@@ -1,3 +1,23 @@
+"""
+Grade para imagem composta de caracteres
+
+Suponha que você tenha uma lista de listas em que cada valor das listas internas seja uma string de um caractere como:
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'], 
+        ['O', 'O', 'O', 'O', 'O', '.'], 
+        ['.', 'O', 'O', 'O', 'O', 'O'], 
+        ['O', 'O', 'O', 'O', 'O', '.'], 
+        ['O', 'O', 'O', 'O', '.', '.'], 
+        ['.', 'O', 'O', '.', '.', '.'], 
+        ['.', '.', '.', '.', '.', '.']]
+
+Podemos pensar em grid[x][y] como sendo o caractere nas coordenadas x e y de uma “imagem” desenhada com caracteres textuais. A origem (0, 0) estará no canto superior esquerdo, as coordenadas x aumentam para a direita e as coordenadas y aumentam para baixo.
+
+"""
+
+
 grid = [['.', '.', '.', '.', '.', '.'],
         ['.', 'O', 'O', '.', '.', '.'],
         ['O', 'O', 'O', 'O', '.', '.'],
