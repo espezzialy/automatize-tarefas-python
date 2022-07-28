4fc0a6f3e59efd4067e1c40e9203950f82e52639
Espezzialy Souza <espezzialy@outlook.com>
2022-07-28 08:41:21 -0300

    finishing book part 1

---
+++ part-i-python-basics/bulletPointerAdder.py
@@ -0,0 +1,15 @@
+#! python3
+# bulletPointAdder.py – Acrescenta marcadores da Wikipedia no início
+# de cada linha de texto do clipboard.
+
+import pyperclip
+text = pyperclip.paste()
+
+# Separa as linhas e acrescenta os asteriscos.
+lines = text.split('\n')
+for i in range(len(lines)): # percorre todos os índices da lista "lines" em um loop
+    lines[i] = '* ' + lines[i] # acrescenta um asterisco em cada string da lista "lines"
+
+text = '\n'.join(lines)
+
+pyperclip.copy(text)
---
+++ part-i-python-basics/pw.py
@@ -0,0 +1,23 @@
+#! python3
+# pw.py = Um programa para repositório de senhas que não é absolutamente nada seguro.
+
+import sys
+import pyperclip
+
+PASSWORDS = {'email': 'F7minlBDDuvMJuxESSKHFhTxFtjVB6',
+'blog': 'VmALvQyKAxiVH5G8v01if1MLZF3sdt',
+'luggage': '12345'}
+
+
+if len(sys.argv) < 2:
+    print('Usage: python pw.py [account] - copy account password')  
+    sys.exit()
+
+
+account = sys.argv[1] # o primeiro argumento da linha de comando é o nome da conta
+
+if account in PASSWORDS:
+    pyperclip.copy(PASSWORDS[account])
+    print('Password for ' + account + ' copied to clipboard.')
+else:
+    print('There is no account named ' + account)
